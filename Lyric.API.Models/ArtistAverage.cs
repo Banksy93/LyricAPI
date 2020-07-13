4b1cdb2ac86a6715ecabe6150c5dd610bf8db99f
stephen.banks <stephen.banks@interact-intranet.com>
2020-07-13 15:33:45 +0100

    Adding ArtistAverage and AverageDetails models

---
+++ Lyric.API.Models/ArtistAverage.cs
@@ -0,0 +1,9 @@
+﻿namespace Lyric.API.Models
+{
+	public class ArtistAverage
+	{
+		public string ArtistId { get; set; }
+		public string ArtistName { get; set; }
+		public AverageDetails AverageDetails { get; set; }
+	}
+}
---
+++ Lyric.API.Models/AverageDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace Lyric.API.Models
+{
+	public class AverageDetails
+	{
+		public double Average { get; set; }
+		public double MaxCount { get; set; }
+		public double MinCount { get; set; }
+	}
+}
