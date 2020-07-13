c8b6fd6c8da6f0d9c9dbf4a207ede953719b1418
stephen.banks <stephen.banks@interact-intranet.com>
2020-07-13 13:07:08 +0100

    Implementing GetSongLyrics to satisfy unit tests

--- LyricService/LyricService.cs
+++ LyricService/LyricService.cs
@@ -1,14 +1,31 @@
 ﻿using System.Threading.Tasks;
 using Lyric.API.Models;
 using Lyrics.Service.Interfaces;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace Lyrics.Service
 {
 	public class LyricService : ILyricService
 	{
+		private const string _baseUrl = "https://api.lyrics.ovh/v1/";
+
 		public async Task<SongLyrics> GetSongLyrics(string artist, string song)
 		{
-			throw new System.NotImplementedException();
+			if (string.IsNullOrEmpty(artist) || string.IsNullOrEmpty(song))
+				return new SongLyrics();
+
+			var request = new RestRequest(_baseUrl + artist + $"/{song}", Method.GET);
+			var client = new RestClient(_baseUrl);
+
+			var response = await client.ExecuteAsync(request);
+
+			if (!response.IsSuccessful)
+				return new SongLyrics();
+
+			var songInfo = JsonConvert.DeserializeObject<SongLyrics>(response.Content);
+
+			return string.IsNullOrEmpty(songInfo.Lyrics) ? new SongLyrics() : songInfo;
 		}
 	}
 }
