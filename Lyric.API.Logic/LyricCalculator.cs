b1c11ea30e550e4086eb7970515f998cf0e0fcaa
stephen.banks <stephen.banks@interact-intranet.com>
2020-07-13 16:53:28 +0100

    Implementing LyricCalculator methods to satisfy unit tests

--- Lyric.API.Logic/LyricCalculator.cs
+++ Lyric.API.Logic/LyricCalculator.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Lyric.API.Logic.Interfaces;
 using Lyric.API.Models;
 
@@ -8,12 +10,27 @@ namespace Lyric.API.Logic
 	{
 		public int GetLyricCount(string songLyrics)
 		{
-			throw new System.NotImplementedException();
+			if (string.IsNullOrEmpty(songLyrics))
+				return 0;
+
+			var escapedLyrics = Regex.Replace(songLyrics, @"\t|\n|\r", " ");
+
+			var lyricArray = escapedLyrics.Split(' ').Where(s => s.Length > 0);
+
+			return lyricArray.Count();
 		}
 
 		public AverageDetails GetLyricCountAverageDetails(IList<int> lyricCounts)
 		{
-			throw new System.NotImplementedException();
+			if (!lyricCounts.Any())
+				return new AverageDetails();
+
+			return new AverageDetails
+			{
+				Average = lyricCounts.Average(),
+				MaxCount = lyricCounts.Max(),
+				MinCount = lyricCounts.Min()
+			};
 		}
 	}
 }
